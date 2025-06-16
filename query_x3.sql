fb7c3910c4337ddeb0f78ffd19a5a8d143b4aff4
ritez-0x53 <riteswarsrgy@gmail.com>
2025-06-16 13:53:17 +0530

    changes in x3

--- query_x3.sql
+++ query_x3.sql
@@ -10,3 +10,53 @@ SELECT MIN(salary) from employee;
 SELECT AVG(salary) from employee;
 SELECT COUNT(id_num) AS total_employee from employee;
 SELECT COUNT(id_num) AS total_desingner from employee WHERE role_id = 105;
+
+# GROUP BY clause
+SELECT role_id from employee GROUP BY role_id;
+
+# count number of employees in a role
+SELECT role_id , count(id_num) from employee GROUP by role_id;
+
+# get highest salary in a role
+SELECT role_id , max(salary) AS highest_salary 
+from employee GROUP BY role_id;
+
+# get lowest salary in each role
+SELECT role_id , min(salary) AS lowest_salary 
+from employee GROUP BY role_id;
+
+# get sum of employee salaries in that role
+SELECT role_id , sum(salary) AS total_sal 
+from employee GROUP BY role_id;
+
+# GROUP BY clause
+SELECT role_id from employee GROUP BY role_id;
+
+# count number of employees in a role
+SELECT role_id , count(id_num) from employee GROUP by role_id;
+
+# get highest salary in a role
+SELECT role_id , max(salary) AS highest_salary 
+from employee GROUP BY role_id;
+
+# get lowest salary in each role
+SELECT role_id , min(salary) AS lowest_salary 
+from employee GROUP BY role_id;
+
+# get sum of employee salaries in that role
+SELECT role_id , sum(salary) AS total_sal 
+from employee GROUP BY role_id;
+
+# count number of employees that get salary less than equal to 35000 in each role
+SELECT role_id,count(salary) from employee Where salary <= 35000 GROUP by role_id;
+
+SELECT role_id , count(id_num) from employee GROUP by role_id HAVING max(salary);
+
+# general commands
+SELECT column(s) 
+FROM table_name 
+WHERE condition
+GROUP BY column(s)
+HAVING condition
+ORDER BY column(s) ASC
+LIMIT number
